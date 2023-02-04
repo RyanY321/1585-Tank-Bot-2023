98e0d79cfdf548ebd946a3b623aa9dee220a093d
Ryan <ryanyoungjohn321@gmail.com>
2023-02-04 16:07:40 -0500

    2/4/2023 Changes
    Added:
    Remove Xbox Drive
    Add Arcade Drive

--- Rocky Robot/src/main/java/frc/robot/Robot.java
+++ Rocky Robot/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -17,21 +21,38 @@ public class Robot extends TimedRobot {
   private DifferentialDrive m_myRobot;
   private final VictorSP m_leftMotor = new VictorSP(0);
   private final VictorSP m_rightMotor = new VictorSP(1);
-  private final XboxController controller = new XboxController(0);
+
+  private final Joystick m_controller = new Joystick(0);
+  //private final XboxController controller = new XboxController(0);
+  
+  //Potentially for future use. This may be used for the gripper. I think we should be able to use a neo brushless with a spark max.
+  //the can option can give us some telemetry (current and/or encoder counts)
+  private final CANSparkMax m_gripperMotor =  new CANSparkMax(0,MotorType.kBrushless);
+
+
   
   @Override
   public void robotInit() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    
+    //m_rightMotor.setInverted(true);
 
     m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(-controller.getLeftY(), -controller.getRightY());
+    m_myRobot.arcadeDrive(-m_controller.getY(), -m_controller.getX()); 
+
+    // m_myRobot.tankDrive(-m_controller.getY(), m_controller.getY());
   }
+
+  // public Boolean CountEncoder()
+  // {
+    
+  // }
+
 }
   
