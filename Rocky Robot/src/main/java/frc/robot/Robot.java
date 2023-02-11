8c8fac01b55a2d7f640b5101e322e18c3e559ed9
Ryan <86565861+RyanY321@users.noreply.github.com>
2023-02-11 15:46:30 -0500

    remove xbox

--- Rocky Robot/src/main/java/frc/robot/Robot.java
+++ Rocky Robot/src/main/java/frc/robot/Robot.java
@@ -6,7 +6,6 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.XboxController;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
